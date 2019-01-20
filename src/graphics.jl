433bb37bd937ea57ed0f4c992476911cd09da3cf
Alice Cecile <alice.i.cecile@gmail.com>
2019-01-20 05:06:03 +0000

    Skeleton for graphics

--- src/graphics.jl
+++ src/graphics.jl
@@ -1,3 +1,42 @@
-using Compose
+import Compose
 
-# draw(SVG("contraption$i.svg", 4cm, 4cm), build_contraption(my_contraption))
+include("structs.jl")
+
+function build(contraption::Contraption)
+    # Point masses ##
+    for i in eachindex(contraption.mass)
+        # Scale point size proportional to cube root of mass
+
+    end
+
+    # Springs ##
+    for i in eachindex(contraption.springs)
+        # Scale base spring width proportional to square root of strength
+        
+
+        # Scale final spring width to conserve total spring volume
+
+    end
+
+    # Combine components
+
+    return "NYI"
+end
+
+function build(bounds::Bounds)
+    # Set corners of box to edge of bounds
+
+
+    return "NYI"
+
+end
+
+function build(contraption::Contraption, bounds::Bounds)
+    bounds_sprite = build(bounds)
+
+    contraption_sprite = build(contraption)
+
+    # Rescale view box to fit the bounds
+
+    return "NYI"
+end
