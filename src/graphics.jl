6d980e737e55e75d8c9353a9de78609dd6ee69d6
Alice Cecile <alice.i.cecile@gmail.com>
2019-01-20 18:50:07 -0500

    Recovery of old graphics code

--- src/graphics.jl
+++ src/graphics.jl
@@ -40,3 +40,40 @@ function build(contraption::Contraption, bounds::Bounds)
 
     return "NYI"
 end
+
+function build_contraption(contraption::Contraption)
+    x = contraption.points[1, :]
+    y = contraption.points[2, :]
+    r = [0.01]
+    
+    X_i = []
+    Y_i = []
+    X_j = []
+    Y_j = []
+    
+    for spring in 1:size(contraption.springs, 2)
+        i, j = contraption.springs[:, spring]
+    
+        x_i, y_i = contraption.points[:, i]
+        x_j, y_j = contraption.points[:, j]
+        
+        push!(X_i, x_i)
+        push!(Y_i, y_i)
+        push!(X_j, x_j)
+        push!(Y_j, y_j)
+    end
+    
+    spring_array = [[(x_i, y_i), (x_j, y_j)] for (x_i, y_i, x_j, y_j) in zip(X_i, Y_i, X_j, Y_j)]
+        
+    obj = compose(context(),
+                  circle(x, y, r),
+                  line(spring_array),
+                  fill("black"),
+                  stroke("black"),
+                  fillopacity(1.),
+                  strokeopacity(0.5))
+
+    return obj
+end
+
+draw(SVG("contraption$i.svg", 4cm, 4cm), build_contraption(my_contraption))
