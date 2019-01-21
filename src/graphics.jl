1de245fdfeba6eba41fc2495059eb46c7d0c5ebf
Alice Cecile <alice.i.cecile@gmail.com>
2019-01-21 00:17:35 -0500

    Basic static plotting of contraptions

--- src/graphics.jl
+++ src/graphics.jl
@@ -1,26 +1,49 @@
-import Compose
+using Compose
 
 include("structs.jl")
 
 function build(contraption::Contraption)
-    # Point masses ##
-    for i in eachindex(contraption.mass)
-        # Scale point size proportional to cube root of mass
-
-    end
-
-    # Springs ##
-    for i in eachindex(contraption.springs)
-        # Scale base spring width proportional to square root of strength
-        
-
-        # Scale final spring width to conserve total spring volume
-
-    end
-
     # Combine components
 
-    return "NYI"
+    # Point masses ##
+    x = contraption.position[1, :]
+    y = contraption.position[2, :]
+    
+    # TODO: Scale point size proportional to cube root of mass
+    r = [0.01]
+    
+    # Springs ##
+    n = length(contraption.mass)
+    X_i, Y_i, X_j, Y_j = [], [], [], []
+
+    for i in 1:n, j in (i+1):n
+        if contraption.springs[i, j] != 0
+            x_i, y_i = contraption.position[:, i]
+            x_j, y_j = contraption.position[:, j]
+
+            push!(X_i, x_i)
+            push!(Y_i, y_i)
+            push!(X_j, x_j)
+            push!(Y_j, y_j)
+
+            # TODO: Scale base spring width proportional to square root of strength
+        
+            # TODO: Scale final spring width to conserve total spring volume
+        end
+    end
+
+    spring_array = [[(x_i, y_i), (x_j, y_j)] for (x_i, y_i, x_j, y_j) in zip(X_i, Y_i, X_j, Y_j)]
+        
+    obj = compose(context(),
+                  circle(x, y, r),
+                  line(spring_array),
+                  fill("black"),
+                  stroke("black"),
+                  fillopacity(1.),
+                  strokeopacity(0.5))
+
+    return obj
+
 end
 
 function build(bounds::Bounds)
@@ -40,40 +63,3 @@ function build(contraption::Contraption, bounds::Bounds)
 
     return "NYI"
 end
-
-function build_contraption(contraption::Contraption)
-    x = contraption.points[1, :]
-    y = contraption.points[2, :]
-    r = [0.01]
-    
-    X_i = []
-    Y_i = []
-    X_j = []
-    Y_j = []
-    
-    for spring in 1:size(contraption.springs, 2)
-        i, j = contraption.springs[:, spring]
-    
-        x_i, y_i = contraption.points[:, i]
-        x_j, y_j = contraption.points[:, j]
-        
-        push!(X_i, x_i)
-        push!(Y_i, y_i)
-        push!(X_j, x_j)
-        push!(Y_j, y_j)
-    end
-    
-    spring_array = [[(x_i, y_i), (x_j, y_j)] for (x_i, y_i, x_j, y_j) in zip(X_i, Y_i, X_j, Y_j)]
-        
-    obj = compose(context(),
-                  circle(x, y, r),
-                  line(spring_array),
-                  fill("black"),
-                  stroke("black"),
-                  fillopacity(1.),
-                  strokeopacity(0.5))
-
-    return obj
-end
-
-draw(SVG("contraption$i.svg", 4cm, 4cm), build_contraption(my_contraption))
