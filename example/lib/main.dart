9535916c1013e70cf73fdcc486f1bfe877e71223
Alice Cecile <alice.i.cecile@gmail.com>
2019-08-24 21:13:43 -0400

    Canvas prototype with CustomPainter

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:positioned_tap_detector/positioned_tap_detector.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart'
     show debugDefaultTargetPlatformOverride;
+import 'package:flutter/semantics.dart';
 
 main() {
   // See https://github.com/flutter/flutter/wiki/Desktop-shells#target-platform-override
@@ -22,7 +23,6 @@ class ContraptionPosition with ChangeNotifier {
   double y = 200;
 
   void spawn(position) {
-    print(position);
     x = position.relative.dx;
     y = position.relative.dy;
     notifyListeners();
@@ -257,23 +257,73 @@ class BuildInterface extends StatelessWidget {
     );
   }
 }
+class Sky extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var rect = Offset.zero & size;
+    var gradient = RadialGradient(
+      center: const Alignment(0.7, -0.6),
+      radius: 0.2,
+      colors: [const Color(0xFFFFFF00), const Color(0xFF0099FF)],
+      stops: [0.4, 1.0],
+    );
+    canvas.drawRect(
+      rect,
+      Paint()..shader = gradient.createShader(rect),
+    );
+  }
 
+  @override
+  SemanticsBuilderCallback get semanticsBuilder {
+    return (Size size) {
+      // Annotate a rectangle containing the picture of the sun
+      // with the label "Sun". When text to speech feature is enabled on the
+      // device, a user will be able to locate the sun on this picture by
+      // touch.
+      var rect = Offset.zero & size;
+      var width = size.shortestSide * 0.4;
+      rect = const Alignment(0.8, -0.9).inscribe(Size(width, width), rect);
+      return [
+        CustomPainterSemantics(
+          rect: rect,
+          properties: SemanticsProperties(
+            label: 'Sun',
+            textDirection: TextDirection.ltr,
+          ),
+        ),
+      ];
+    };
+  }
+
+  // Since this Sky painter has no fields, it always paints
+  // the same thing and semantics information is the same.
+  // Therefore we return false here. If we had fields (set
+  // from the constructor) then we would return true if any
+  // of them differed from the same fields on the oldDelegate.
+  @override
+  bool shouldRepaint(Sky oldDelegate) => false;
+  @override
+  bool shouldRebuildSemantics(Sky oldDelegate) => false;
+}
 class BuildDisplay extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
     return(
-      Container(
-        width: 400,
-        height: 400,
-        decoration: BoxDecoration(
-          border: Border.all(width: 2),
-        ),
-        child: PositionedTapDetector(
-          child: Consumer<ContraptionPosition>(
-            builder: (context, contraption, child) => Text('${contraption.x}, ${contraption.y}')
+      CustomPaint(
+        painter: Sky(),
+        child: Container(
+          width: 400,
+          height: 400,
+          decoration: BoxDecoration(
+            border: Border.all(width: 2),
           ),
-          onTap: (position) => Provider.of<ContraptionPosition>(context, listen: false).spawn(position)
+          child: PositionedTapDetector(
+            child: Consumer<ContraptionPosition>(
+              builder: (context, contraption, child) => Text('${contraption.x}, ${contraption.y}')
+            ),
+            onTap: (position) => Provider.of<ContraptionPosition>(context, listen: false).spawn(position)
+          )
         )
       )
     );
@@ -388,7 +438,6 @@ class LearnTab extends StatelessWidget {
     );
   }
 }
-
 
 class LearnInterface extends StatelessWidget {
   @override
